fdee1a30b11a1e4da9853b2f3c78272a681b3d81
henrytech <amuchiehenry60@gmail.com>
2025-01-18 10:34:33 +0100

    Create template and render html file. Show how to make html file dynamic

--- calc/views.py
+++ calc/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-  return HttpResponse('<h1>Hello world, thank you</h1>')
+  return render(request,'home.html',{'name': 'Henry'})
 
 
