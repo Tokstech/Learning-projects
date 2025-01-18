f28961a7acd0a0a591fb915b4348b7c7270c23ba
henrytech <amuchiehenry60@gmail.com>
2025-01-18 15:11:45 +0100

    Addition of two numbers with GET request

--- calc/views.py
+++ calc/views.py
@@ -7,3 +7,10 @@ def home(request):
   return render(request,'home.html',{'name': 'Henry'})
 
 
+def add(request):
+  val1 = int(request.GET['num1'])
+  val2 = int(request.GET['num2'])
+
+
+  result = val1 + val2
+  return render(request,'result.html', {'result': result})
