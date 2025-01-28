f2e1cafc58d09f7fc56b8d35a61456f8acb0bd5d
henrytech <amuchiehenry60@gmail.com>
2025-01-28 23:12:14 +0100

    User logout and Destination mapping

--- Tochukwu/urls.py
+++ Tochukwu/urls.py
@@ -23,7 +23,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('travello.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('destination/', include('destination.urls'))
+
 ] 
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
