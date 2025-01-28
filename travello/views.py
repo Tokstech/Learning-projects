449bed7b9340c0c714c8c77a6932a407c025a601
henrytech <amuchiehenry60@gmail.com>
2025-01-28 15:29:02 +0100

    models, migratio and fetching data from database

--- travello/views.py
+++ travello/views.py
@@ -6,37 +6,7 @@ from .models import Destination
 
 def index(request):
 
-  dest1 = Destination()
-  dest1.name = 'Lagos'
-  dest1.desc = 'Busy City'
-  dest1.img = 'destination_1.jpg'
-  dest1.price = 500
-  dest1.offer = False
-
-  dest2 = Destination()
-  dest2.name = 'Ibadan'
-  dest2.desc = 'Hotel City'
-  dest2.img = 'destination_2.jpg'
-  dest2.price = 400
-  dest2.offer = True
-
-  dest3 = Destination()
-  dest3.name = 'Port Harcout'
-  dest3.desc = 'Bole City'
-  dest3.img = 'destination_3.jpg'
-  dest3.price = 450
-  dest3.offer = False
-
-  dest4 = Destination()
-  dest4.name = 'Owerri'
-  dest4.desc = 'Hotel City'
-  dest4.img = 'destination_2.jpg'
-  dest4.price = 650
-  dest4.offer = True
-
-
-  dests = [dest1, dest2, dest3, dest4]
-
+  dests = Destination.objects.all()
   
    
   return render(request,'index.html', {'dests': dests})  
