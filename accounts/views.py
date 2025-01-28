30a2db02ee259d239cf2beae0c9acd0e02167014
henrytech <amuchiehenry60@gmail.com>
2025-01-28 22:14:00 +0100

    User login

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,24 @@ from django.contrib import messages
 
 # Create your views here.
 
+def login(request):
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+      auth.login(request, user)
+      return redirect('/')
+    
+    else:
+      return render(request, 'login.html', {'error': 'Invalid Credentials or account not Found, <a href="/accounts/register">Create account here</a>'})
+
+    
+  else:
+    return render(request, 'login.html')
+
 def register(request):
 
   if request.method == 'POST':
@@ -25,7 +43,7 @@ def register(request):
         user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name,last_name=last_name)
         user.save();
         print ('user created')
-        return redirect('/')
+        return redirect('login')
     else:
       print('password not matching...')
       return render(request, 'register.html', {'error': 'Passwords do not match!'})
